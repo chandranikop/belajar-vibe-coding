7e837fe0c57d52cf547a681eef69f47d53d33a49
Chandra Niko P <nikochandra3@gmail.com>
2026-04-04 04:01:19 +0700

    feat: implement unit test for all user apis (#13)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Elysia } from "elysia";
 import { usersRoute } from "./routes/users-route";
 
-const app = new Elysia()
+export const app = new Elysia()
   .use(usersRoute)
   .get("/", () => "Welcome to Bun + ElysiaJS + Drizzle + MySQL")
-  .get("/health", () => ({ status: "ok" }))
-  .listen(process.env.PORT || 3000);
+  .get("/health", () => ({ status: "ok" }));
+
+app.listen(process.env.PORT || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
